0c1ad2ba199f05c866bfd21a85bb406a52f3c5fe
Vitalii Kyi <kyi.profy@gmail.com>
2023-06-15 19:55:41 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul');
+
+gallery.style.listStyle = 'none';
+
+const addItems = galleryItems.map((item) =>
+  `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>
+</li>`
+).join('');
+
+gallery.insertAdjacentHTML('afterbegin', addItems);
+
+let lightbox = new SimpleLightbox('.gallery li a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  caoptionDelay: 250,
+  scrollZoom: true,
+});
