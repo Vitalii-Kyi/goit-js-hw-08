92393ce422d8363d8f4d80497ec27714b55220cd
Vitalii Kyi <kyi.profy@gmail.com>
2023-06-17 07:17:11 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+document.addEventListener('DOMContentLoaded', restorePlaybackTime);
+
+player.on('timeupdate', throttle(handleTimeUpdate, 1000));
+
+function handleTimeUpdate(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time');
+}
+
+function restorePlaybackTime() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+}
